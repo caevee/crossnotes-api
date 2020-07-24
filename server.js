e2f37523a3679d5bc2154001c8d450cf76d4741e
caevee <pister.alex.cv@gmail.com>
2020-07-24 21:47:21 +0200

    bodyparser fix

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(cors());
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });*/
+app.use(bodyParser.json())
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Mongo Connection
