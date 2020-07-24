bf5fbb984d96dc9f094f19be96669a49b153e5eb
caevee <pister.alex.cv@gmail.com>
2020-07-24 20:56:48 +0200

    CORS fix

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 /*const { mongosecret } = require(__dirname + "/secrets.js");*/
 const { v4: uuidv4 } = require("uuid");
 
 const mongosecret = process.env.MONGOSECRET;
 const port = process.env.PORT || 3001;
 const app = express();
-app.use(function(req, res, next) {
+app.use(cors());
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Mongo Connection
